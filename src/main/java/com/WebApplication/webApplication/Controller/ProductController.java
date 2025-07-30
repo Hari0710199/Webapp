075a98a1e617acc6bffede0cdca1e6452edd09e7
Harikrishna <rajollakrishnareddy16@gmail.com>
2025-07-30 22:33:19 +0530

    added put and delete without JPA

--- src/main/java/com/WebApplication/webApplication/Controller/ProductController.java
+++ src/main/java/com/WebApplication/webApplication/Controller/ProductController.java
@@ -3,9 +3,11 @@ package com.WebApplication.webApplication.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,8 @@ public class ProductController {
 	@Autowired
 	ProductService productService;
 
-	@RequestMapping("/products")
+	@GetMapping("/products")
 	public List<Product> getProcucts() {
-		System.out.println("Inside Controller");
 		return productService.getListOfProducts();
 	}
 
@@ -36,5 +37,15 @@ public class ProductController {
 	public void addProduct(@RequestBody Product product) {
 		System.out.println(product);
 		productService.addProduct(product);
+	}
+
+	@PutMapping("/products/update")
+	public void upadateProduct(@RequestBody Product product) {
+		productService.updateProduct(product);
+	}
+
+	@DeleteMapping("/products/delete/{id}")
+	public void deleteProduct(@PathVariable int id) {
+		productService.deleteProduct(id);
 	}
 }
